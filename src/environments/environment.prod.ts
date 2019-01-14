e87a7d17bd5a582c86d42952fd52b125f6519d40
sidinsomniac <chatterjeesid1993@gmail.com>
2019-01-14 13:57:04 +0530

    Add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDxWaqTNNtJs8QMRzKOrr126B5JqGkF1JU",
+    authDomain: "online-shop-e2b6d.firebaseapp.com",
+    databaseURL: "https://online-shop-e2b6d.firebaseio.com",
+    projectId: "online-shop-e2b6d",
+    storageBucket: "online-shop-e2b6d.appspot.com",
+    messagingSenderId: "734429567805"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDxWaqTNNtJs8QMRzKOrr126B5JqGkF1JU",
+    authDomain: "online-shop-e2b6d.firebaseapp.com",
+    databaseURL: "https://online-shop-e2b6d.firebaseio.com",
+    projectId: "online-shop-e2b6d",
+    storageBucket: "online-shop-e2b6d.appspot.com",
+    messagingSenderId: "734429567805"
+  }
 };
 
 /*
