c0fd6a0eae6fca25acff111b937ac5921a8be789
sidinsomniac <chatterjeesid1993@gmail.com>
2019-01-14 17:59:19 +0530

    Make components and add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,45 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { LoginComponent } from './login/login.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent },
+  { path: 'check-out', component: CheckOutComponent },
+  { path: 'order-success', component: OrderSuccessComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'admin/products', component: AdminProductsComponent },
+  { path: 'admin/orders', component: AdminOrdersComponent },
+];
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
+
 export class AppRoutingModule { }
+
+export const routingComponents = [
+  HomeComponent,
+  ProductsComponent,
+  ShoppingCartComponent,
+  CheckOutComponent,
+  OrderSuccessComponent,
+  LoginComponent,
+  AdminProductsComponent,
+  AdminOrdersComponent
+]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire'
 import { AngularFireDatabaseModule } from '@angular/fire/database'
-import { AngularFireAuthModule } from '@angular/fire/auth'
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
-import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
+import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { routingComponents, AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MyOrdersComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
