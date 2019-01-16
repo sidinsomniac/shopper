e6f6df212cc548d692c9bc6222c2b56c93140eb4
sidinsomniac <chatterjeesid1993@gmail.com>
2019-01-16 17:41:09 +0530

    Set up new product and product list page

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from 'src/app/category.service';
 import { ProductService } from 'src/app/product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
@@ -12,7 +13,8 @@ export class ProductFormComponent implements OnInit {
   categories$;
 
   constructor(
-    categoryService: CategoryService,
+    private categoryService: CategoryService,
+    private router: Router,
     private productService: ProductService
     ) {
     this.categories$ = categoryService.getCategories();
@@ -23,6 +25,7 @@ export class ProductFormComponent implements OnInit {
 
   save(product) {
     this.productService.create(product);
+    this.router.navigate(['/admin/products']);
   }
 
 }
